bd33865bfa2e2635270964dde90f7399204f017c
Marco <kuvimarco@gmail.com>
2020-08-27 09:47:18 +0200

    Add AboutMe Class

--- src/AboutMe.java
+++ src/AboutMe.java
@@ -1,6 +1,14 @@
 
 public class AboutMe {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        String forname = "Marco";
+        String surname = "Kuvi";
+        String birthday = "23.05.2000";
+        int age = 20;
+        char gender = 'm';
+        double averageGrade = 2.6;
+        boolean status = false;
+
+        System.out.println(" My name is " + forname + " " + surname + " and i'm born on the " + birthday + " i'm " + age + " years old, my gender is " + gender + " my marrriage status is " + status + " and my average grades is " + averageGrade);
     }
 }
