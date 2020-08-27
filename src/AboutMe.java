594496c7001c283ef8e7b987387c78a3ffa294af
Marco <kuvimarco@gmail.com>
2020-08-27 11:29:03 +0200

    Add AboutMe Class

--- src/AboutMe.java
+++ src/AboutMe.java
@@ -1,14 +1,20 @@
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class AboutMe {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         String forname = "Marco";
         String surname = "Kuvi";
-        String birthday = "23.05.2000";
         int age = 20;
         char gender = 'm';
         double averageGrade = 2.6;
         boolean status = false;
 
-        System.out.println(" My name is " + forname + " " + surname + " and i'm born on the " + birthday + " i'm " + age + " years old, my gender is " + gender + " my marrriage status is " + status + " and my average grades is " + averageGrade);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        Date birthday = simpleDateFormat.parse("23.05.2000");
+
+        System.out.println(" My name is " + forname + " " + surname + " and i'm born on the " + (simpleDateFormat.format(birthday)) + " i'm " + age + " years old, my gender is " + gender + " my marrriage status is " + status + " and my average grades is " + averageGrade);
     }
 }
